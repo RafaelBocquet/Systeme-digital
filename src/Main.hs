864146e67211241db6ed6d03a1eb43d674e6587b
Arthur Carcano <arthur.carcano@orange.fr>
2014-10-13 18:39:08 +0200

    main tets différent

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,10 @@
 import Lexer
 import Parser
+import Scheduler
 import System.Environment
 import System.IO
 
-main =
+testmain1 =
   do file:_ <- getArgs
      handle <- openFile file ReadMode
      content <- hGetContents handle
@@ -12,3 +13,10 @@ main =
      print "****"
      print $ parse $ (alexScanTokens content)
      return ()
+
+main =
+  do file: <- getArgs
+     handle <- openfile file ReadMode
+     content <- hGetContents handle
+     let netl = parse $ (alexScanTokens content)
+     print $ scheduler netl
