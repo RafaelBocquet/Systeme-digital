5b64355e1474d0013c23281d8d3c39c53c95fcf3
Arthur Carcano <arthur.carcano@orange.fr>
2014-11-03 03:52:09 +0100

    Added check for cycle

--- src/DepGraph.hs
+++ src/DepGraph.hs
@@ -47,13 +47,15 @@ cycleDetection graph =
 
 topoSortWithState :: Graph -> Label -> State ((M.Map Label Mark),[Label]) () 
 topoSortWithState graph l =
-  do (marks,_) <- get
-     if fromMaybe NotVisited (M.lookup l marks) == Visited
-       then return ()
-       else do let children = fromMaybe [] (S.toList <$> (M.lookup l graph))
-               _ <- sequence $ map (topoSortWithState graph) children
-               modify $ (M.insert l Visited) *** (l:)
-               return ()
+  if (cycleDetection graph) then error "Circular Dependency, aborting."
+  else
+    do (marks,_) <- get
+       if fromMaybe NotVisited (M.lookup l marks) == Visited
+         then return ()
+         else do let children = fromMaybe [] (S.toList <$> (M.lookup l graph))
+                 _ <- sequence $ map (topoSortWithState graph) children
+                 modify $ (M.insert l Visited) *** (l:)
+                 return ()
 
 reverseGraph :: Graph -> Graph
 reverseGraph graph =
